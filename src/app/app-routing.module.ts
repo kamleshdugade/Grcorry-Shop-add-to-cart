c3a5b5a04c072fb0951c8560cd4d4a34fee7c533
kamleshdugade <kamlesh.dugade20@vit.edu>
2021-12-22 12:50:07 +0530

    Commit pipes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ProductsComponent} from "./component/products/products.component";
 import {CartComponent} from "./component/cart/cart.component";
+import {AppComponent} from "./app.component";
 
 const routes: Routes = [
   {path:'', redirectTo:'products',pathMatch:'full'},
   {path: 'products', component:ProductsComponent},
-  {path:'cart', component:CartComponent}
+  {path:'cart', component:CartComponent},
+  {path:'home', component:AppComponent}
 ];
 
 @NgModule({
